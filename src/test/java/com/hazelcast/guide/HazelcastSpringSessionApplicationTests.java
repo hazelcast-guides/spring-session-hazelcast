51b63ba7e2ba6046460c491ecf28ffac6d29daea
Enes Ozcan <enes.ozcan@hazelcast.com>
2020-12-01 16:40:22 +0300

    Add guide.

---
+++ src/test/java/com/hazelcast/guide/HazelcastSpringSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazelcast.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastSpringSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
