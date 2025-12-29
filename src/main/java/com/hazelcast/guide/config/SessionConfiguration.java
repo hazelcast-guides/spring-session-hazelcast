8ca5d2934ef4b47cedf456532d4fad01dc94953f
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2025-12-29 20:10:09 +0100

    Update guide to use new module

--- src/main/java/com/hazelcast/guide/config/SessionConfiguration.java
+++ src/main/java/com/hazelcast/guide/config/SessionConfiguration.java
@@ -2,26 +2,24 @@ package com.hazelcast.guide.config;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.config.AttributeConfig;
 import com.hazelcast.config.Config;
-import com.hazelcast.config.IndexConfig;
-import com.hazelcast.config.IndexType;
-import com.hazelcast.config.SerializerConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.session.HazelcastIndexedSessionRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.session.FlushMode;
-import org.springframework.session.MapSession;
 import org.springframework.session.SaveMode;
-import org.springframework.session.Session;
 import org.springframework.session.config.SessionRepositoryCustomizer;
-import org.springframework.session.hazelcast.Hazelcast4IndexedSessionRepository;
-import org.springframework.session.hazelcast.Hazelcast4PrincipalNameExtractor;
-import org.springframework.session.hazelcast.Hazelcast4SessionUpdateEntryProcessor;
-import org.springframework.session.hazelcast.HazelcastSessionSerializer;
-import org.springframework.session.hazelcast.config.annotation.SpringSessionHazelcastInstance;
-import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import com.hazelcast.spring.session.config.annotation.SpringSessionHazelcastInstance;
+import com.hazelcast.spring.session.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import java.time.Duration;
+
+import static com.hazelcast.spring.session.HazelcastSessionConfiguration.applySerializationConfig;
 
 @Configuration
 @EnableHazelcastHttpSession
@@ -30,52 +28,32 @@ class SessionConfiguration {
     private final String SESSIONS_MAP_NAME = "spring-session-map-name";
 
     @Bean
-    public SessionRepositoryCustomizer<Hazelcast4IndexedSessionRepository> customize() {
+    public SessionRepositoryCustomizer<HazelcastIndexedSessionRepository> customize() {
         return (sessionRepository) -> {
             sessionRepository.setFlushMode(FlushMode.IMMEDIATE);
             sessionRepository.setSaveMode(SaveMode.ALWAYS);
             sessionRepository.setSessionMapName(SESSIONS_MAP_NAME);
-            sessionRepository.setDefaultMaxInactiveInterval(900);
+            sessionRepository.setDefaultMaxInactiveInterval(Duration.ofMinutes(2));
         };
     }
 
     @Bean
-    @SpringSessionHazelcastInstance
+    @ConditionalOnExpression("${guide.useClientServer:false} == false")
     public HazelcastInstance hazelcastInstance() {
         Config config = new Config();
-        config.setClusterName("spring-session-cluster");
+        config.getNetworkConfig().getJoin().getTcpIpConfig()
+              .setEnabled(true)
+              .addMember("127.0.0.1");
 
-        // Add this attribute to be able to query sessions by their PRINCIPAL_NAME_ATTRIBUTE's
-        AttributeConfig attributeConfig = new AttributeConfig()
-                .setName(Hazelcast4IndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
-                .setExtractorClassName(Hazelcast4PrincipalNameExtractor.class.getName());
-
-        // Configure the sessions map
-        config.getMapConfig(SESSIONS_MAP_NAME)
-                .addAttributeConfig(attributeConfig).addIndexConfig(
-                new IndexConfig(IndexType.HASH, Hazelcast4IndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE));
-
-        // Use custom serializer to de/serialize sessions faster. This is optional.
-        // Note that, all members in a cluster and connected clients need to use the
-        // same serializer for sessions. For instance, clients cannot use this serializer
-        // where members are not configured to do so.
-        SerializerConfig serializerConfig = new SerializerConfig();
-        serializerConfig.setImplementation(new HazelcastSessionSerializer()).setTypeClass(MapSession.class);
-        config.getSerializationConfig().addSerializerConfig(serializerConfig);
-
-        return Hazelcast.newHazelcastInstance(config);
+        return Hazelcast.newHazelcastInstance(applySerializationConfig(config));
     }
 
-    /* Hazelcast Client Instance Bean
     @Bean
-    @SpringSessionHazelcastInstance
-    public HazelcastInstance hazelcastInstance() {
+    @ConditionalOnExpression("${guide.useClientServer:false}")
+    public HazelcastInstance hazelcastClient() {
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
-        clientConfig.getUserCodeDeploymentConfig().setEnabled(true).addClass(Session.class)
-                .addClass(MapSession.class).addClass(Hazelcast4SessionUpdateEntryProcessor.class);
-        return HazelcastClient.newHazelcastClient(clientConfig);
+        return HazelcastClient.newHazelcastClient(applySerializationConfig(clientConfig));
     }
-    */
 
 }
